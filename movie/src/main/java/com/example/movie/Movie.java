509a109006c7633e08f7d8795d625cc00cc3391c
Ramesh <rameshrmrama@gmail.com>
2025-07-13 13:47:23 +0000

    adding movie project

---
+++ movie/src/main/java/com/example/movie/Movie.java
@@ -0,0 +1,43 @@
+package com.example.movie;
+
+public class Movie {
+    private int movieId;
+    private String movieName;
+    private String leadActor;
+
+    // Constructor
+    public Movie(int movieId, String movieName, String leadActor) {
+        this.movieId = movieId;
+        this.movieName = movieName;
+        this.leadActor = leadActor;
+    }
+
+    // Default constructor
+    public Movie() {
+    }
+
+    // Getters and Setters
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getLeadActor() {
+        return leadActor;
+    }
+
+    public void setLeadActor(String leadActor) {
+        this.leadActor = leadActor;
+    }
+}
